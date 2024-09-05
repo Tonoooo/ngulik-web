d072b346a476085b0dd449f1aa4c67cec0ff2cbe
tonoooo <adomtonot@gmail.com>
2024-09-05 20:47:14 +0700

    pengenalan sampai jendela dialog.

---
+++ javascript/dasar_petanikode/pengenalan/javascriptnya.js
@@ -0,0 +1,11 @@
+// alert("Hello World");
+
+// document.write("belajar js mang"); // mencetak ke dalam dokumen html "belajar js mang"
+
+// --- variabel
+let nama = "Mang";
+let umur = 20;
+let alamat = "Jl. Kebon Jeruk"; 
+
+let dil = false;
+
